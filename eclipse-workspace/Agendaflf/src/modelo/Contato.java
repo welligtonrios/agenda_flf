6c18493b70fe6ec4c8b0e9981cd695970a05c20c
wrios <wriosfaculdade@gmail.com>
2018-09-20 11:32:41 -0300

    Criado a classe contato. Criado a classe contatoService.(ArrayList). Criado metodos(adiconar, remover, criar, deletar contatos).

---
+++ eclipse-workspace/Agendaflf/src/modelo/Contato.java
@@ -0,0 +1,44 @@
+package modelo;
+
+
+
+
+public class Contato {
+	
+	private String nome;
+	private long tel;
+	
+ 
+	public Contato() {
+		
+	}
+	
+	    	public Contato(String nome, long tel) {
+	    		this.nome = nome ; 
+	    		this.tel = tel ; 
+	    	}
+
+			public String getNome() {
+				return nome;
+			}
+
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+
+			public long getTel() {
+				return tel;
+			}
+
+			public void setTel(long tel) {
+				this.tel = tel;
+			}
+			
+			
+	    	
+	    	public void mostreContato(){
+	    		System.out.println("nome : " + this.nome);
+	    		System.out.println("tel : " + this.tel);
+	    	}
+	    	
+}
---
+++ eclipse-workspace/Agendaflf/src/service/ContatoService.java
@@ -0,0 +1,68 @@
+package service;
+
+import java.util.ArrayList;
+
+import modelo.Contato;
+
+public class ContatoService {
+
+	//atributos
+	
+	ArrayList<Contato> listaContato = new ArrayList<Contato>();
+	
+	
+	public ArrayList<Contato>getListContato(){
+	  return this.listaContato; 
+		
+	}
+	
+	//CRUD
+	
+	
+	//Criar contato
+	
+	public void addContato(String nome, long tel) {
+		Contato c = new Contato(nome, tel);
+		listaContato.add(c);
+	}
+	   
+	//pesquisar
+	
+	 public Contato buscaContato(String nome) {
+		 for (Contato contato : listaContato) {
+			if (contato.getNome()==nome) {
+				return contato ; 
+			}
+		}
+		 System.out.println("contato nao encontrado...");
+		 return null;
+	 }
+	 
+	 // Atualizar contato
+	 
+	 public void configContato (Contato obj,String nome ) {
+		 obj.setNome(nome);
+	 }
+	 public void configContato (Contato obj, long tel ) {
+		 obj.setTel(tel);
+	 }
+	 public void configContato (Contato obj,String nome, long tel) {
+		 obj.setNome(nome);
+		 obj.setTel(tel);
+	 }
+	 // deletar contato
+	 
+	 public void deleteContato(String nome) {
+		 listaContato.remove(buscaContato(nome));
+	 }
+	 
+	 //Lista Contato
+	 
+	 public void listarContato() {
+		 
+		 for (Contato contato : listaContato) {
+			contato.mostreContato();
+		}
+	 }
+	
+}
